0203d4dff7b0705147e788af673d04ac5ae86971
Hanif <mhanifsaadeij@gmail.com>
2019-12-29 18:08:48 +0330

    initial models

--- src/com/company/Controller/Main.java
+++ src/com/company/Controller/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Menu menuLocation = Menu.MAIN_MENU;
+        System.out.println("hanif");
     }
 }
